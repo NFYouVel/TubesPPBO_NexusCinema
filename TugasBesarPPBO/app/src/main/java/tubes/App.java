cbe0adea6cd0282a50e369ee54e12419dbff801d
Marvel Nathanael <marvelnathanael072006@gmail.com>
2025-11-26 16:02:37 +0700

    Change First commit

--- TugasBesarPPBO/app/src/main/java/tubes/App.java
+++ TugasBesarPPBO/app/src/main/java/tubes/App.java
@@ -5,7 +5,7 @@ package tubes;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello Jennie!";
     }
 
     public static void main(String[] args) {
